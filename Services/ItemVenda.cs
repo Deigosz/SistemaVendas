f7efaec9981d9661ffa76de71ebcaf22b9984547
Diego Rodrigues <rodriguesdiego412@gmail.com>
2024-06-09 14:10:01 -0300

    Adicionar arquivos de projeto.

---
+++ Services/ItemVenda.cs
@@ -0,0 +1,18 @@
+﻿namespace SistemaVendas.Services
+{
+    public class ItemVenda
+    {
+        private static int proximoId = 1;
+
+        public int Id { get; set; }
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+
+        public ItemVenda(Produto produto, int Quantidade) 
+        {
+            this.Id = proximoId++;
+            this.Produto = produto;
+            this.Quantidade = Quantidade;
+        }
+    }
+}
---
+++ Services/Venda.cs
@@ -0,0 +1,28 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Services
+{
+    public class Venda
+    {
+        private static int proximoId = 1;
+
+        public int Id { get; private set; }
+        public DateTime Data { get; private set; }
+        private double Total { get; }
+        public List<ItemVenda> Itens { get; private set; }
+
+        public Venda(DateTime) 
+        {
+            this.Data = DateTime.Now;
+        }
+
+
+
+    }
+}
+*/
